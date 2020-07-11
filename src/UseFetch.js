08057b3aa5ff7ac88adf91331fb4bbc8269918a9
NicKAnderson038 <andersonnick038@gmail.com>
2020-07-10 21:11:28 -0400

    🎨

---
+++ src/UseFetch.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+
+const useFetch = (url, options) => {
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const res = await fetch(url, options);
+        const json = await res.json();
+        setResponse(json);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false)
+      }
+    };
+    fetchData();
+  }, []);
+  return { response, error, isLoading };
+};
+
+export default useFetch
